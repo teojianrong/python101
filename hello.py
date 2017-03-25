db70165b5593ba1b9e68cdcb40e40f1cc5998750
Teo Jian Rong <teojianrong@gmail.com>
2017-03-25 14:50:53 +0800

    new codes
    operators and others

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello world")
+#this is a comment
+#print("hello world")
+
+'''
+str = "hello"
+
+print(str)
+print(str[0:1])
+print(str[0:-2])
+print(str[0:-3])
+'''
+'''
+a = "glue "
+b = "togather"
+c = a+b
+c = c * 1000
+print(c)
+'''
+a = 8
+b = 7
+c = a % b
+print(c)
